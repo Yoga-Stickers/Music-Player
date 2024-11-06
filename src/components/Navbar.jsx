33828128ff7f9e18d09d3d9d5e4b804502a3e5eb
SHravuSM <SHravansm169@gmail.com>
2024-11-06 23:55:26 +0530

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,29 +13,29 @@ export default function Navbar({ songs, select }) {
     // console.log(search);
 
     return (
-        <div className='bg-black flex items-center justify-between lg:h-[15vh] lg:pr-2 lg:pl-2 sm:pl-1 sm:pr-1 w-full h-[20vh]'>
+        <div className='bg-slate-600 flex items-center justify-center lg:h-[15vh] sm:h-[12vh] lg:pr-2 lg:pl-2 sm:pl-1 sm:pr-1 w-full'>
 
-            <div className='h-full flex items-center '>
-                <img className='sm:h-12 md:h-20 lg:w-24 cursor-pointer' src={guitar} alt="" />
+            <div className='h-full sm:pl-1 flex items-center '>
+                <img className='sm:h-14 sm:w-16 md:h-20 lg:w-24 cursor-pointer' src={guitar} alt="" />
             </div>
 
-            <div className='w-full flex justify-end lg:gap-48 items-center'>
+            <div className='w-full flex justify-end lg:gap-48 sm:gap-1  items-center'>
 
-                <div className='flex w-[45%] relative items-center justify-end lg:h-14 sm:h-9'>
-                    <div className='w-full gap-1 flex justify-center items-center  h-full'>
-                        
-                        <div className='h-12 w-[15%]  flex items-center justify-center lg:w-12 sm:w-10 bg-slate-800 rounded-md'>
-                            <img className='h-7 rounded-lg cursor-pointer' src={home_30} alt="" />
+                <div className='flex lg:w-[45%] sm:w-[65%] relative items-center justify-end lg:h-14 sm:h-9'>
+                    <div className='w-full gap-1 flex justify-center items-center lg:h-full  sm:h-[100%]'>
+
+                        <div className='lg:h-12 sm:h-8 flex items-center justify-center lg:w-12 sm:w-9 bg-slate-800 rounded-md'>
+                            <img className='lg:h-7 sm:h-5 rounded-lg cursor-pointer' src={home_30} alt="" />
                         </div>
 
-                        <div className='w-[85%] h-12 active: active:-white flex items-center rounded-md bg-slate-800'>
+                        <div className='w-[90%] lg:h-12 sm:h-10 border sm:pr-2 justify-around flex items-center rounded-md bg-slate-800'>
                             <input
                                 onChange={(e) => setSearch(e.target.value)}
                                 value={search}
-                                className='w-[85%] rounded-md active:-none h-full placeholder:text-slate-400 lg:pl-5 sm:pl-1 font-semibold bg-slate-800  ' placeholder='What do you want to play?' type="text" />
-                            <hr className='w-8 rotate-90  text-red-600' />
-                            <img className='lg:w-7 cursor-pointer' src={Search} alt="" />
-                        
+                                className='w-[85%] text-white rounded-md active:-none h-full placeholder:text-slate-400 lg:pl-5 sm:pl-1 font-semibold bg-slate-800 sm:placeholder:text-sm sm:font-normal ' placeholder='Search Songs here' type="text" />
+                            <hr className='lg:w-8 sm:w-5 rotate-90 text-red-600' />
+                            <img className='lg:w-7 sm:h-5 sm:w-5 cursor-pointer' src={Search} alt="" />
+
                         </div>
                     </div>
                     {search == '' ? <></> : <div className='z-10 flex flex-col gap-2 pl-3 overflow-scroll p-0 overflow-y-visible h-[375px] absolute top-[74px] rounded-md w-[750px]'>
@@ -48,9 +48,9 @@ export default function Navbar({ songs, select }) {
                     </div>}
                 </div>
 
-                <div className='lg:w-[15%] sm:w-[25%] flex lg:h-full sm:h-10 items-center gap-1 rounded-md justify-evenly '>
-                    <h1 className='text-slate-400 rounded-3xl sm:text-sm font-sans font-semibold'>Join</h1>
-                    <img src='#' alt='USER' className='bg-white lg:p-3 rounded-md lg:w-24 sm:w-12 flex sm:text-[14px] cursor-pointer justify-center items-center font-bold' />
+                <div className='lg:w-[15%] sm:w-[30%] flex flex-col lg:h-full sm:h-12 items-center rounded-md '>
+                    <img src={home_30} className=' bg-none lg:p-3 object-contain object-center  rounded-md lg:w-24 sm:size-9 flex sm:text-[7px] cursor-pointer justify-center items-center font-bold' />
+                    <h1 className='text-white rounded-3xl sm:text-sm font-sans font-semibold'>Shravankumar</h1>
                 </div>
 
             </div>
